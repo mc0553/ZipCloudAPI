d020d8e6c0a69425701de13536c739b8a698a2f9
nonteku <akamckokou@gmail.com>
2019-05-23 22:30:12 +0900

    no message

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,26 @@
 
-$(function() {
-
+$(function() {   
     $('#search-btn').on('click', function() {
-
         //入力欄を取得
-        let inputTask = $('#search-word').val();
-
+        let input = $('#search-word').val();
         //ajaxで通信を開始して住所をgetしにいく
         $.ajax({
-         url: 'http://zipcloud.ibsnet.co.jp/api/search',
-         type: 'GET',
-         datatype: 'jsonp',
-         data:{
-            zipcode: inputTask,
-         }
-
+            url: 'http://zipcloud.ibsnet.co.jp/api/search',
+            type: 'GET',    // GET送信 or POST送信
+            dataType: 'jsonp',
+            data:{
+                zipcode: input,
+            }
         }).done( (data) => {
             console.log(data);
 
-            // $('#prefecture').text('');
-            // $('#city').text('');
-            // $('#address').text('');
-
-            // $('#prefecture').text(data.results.address1);
-            // $('#city').text(data.results.address2);
-            // $('#address').text(data.results.address3);
+            $('#prefecture').text(data.results[0].address1);
+            $('#city').text(data.results[0].address2);
+            $('#address').text(data.results[0].address3);
 
         }).fail( (error) => {
-        
+            console.log(error);
+            
         })
-    
     })
-
 })
